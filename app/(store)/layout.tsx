edc0d94d7819f032df7bbe1c095ec5eb4f827055
Riptwosec-collab <aidsaras@gmail.com>
2026-08-03 19:42:35 +0700

    fix provider hierarchy for storefront

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -1,12 +1,15 @@
 import { Header } from "@/components/layout/header";
 import { Footer } from "@/components/layout/footer";
+import { Providers } from "@/components/shared/providers";
 
 export default function StoreLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen flex-col">
-      <Header />
-      <main className="flex-1">{children}</main>
-      <Footer />
-    </div>
+    <Providers>
+      <div className="flex min-h-screen flex-col">
+        <Header />
+        <main className="flex-1">{children}</main>
+        <Footer />
+      </div>
+    </Providers>
   );
 }
